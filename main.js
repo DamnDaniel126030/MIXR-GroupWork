36788128f80b47393c0caef6d8c9ace7e611e9e9
Fruvvien <HunFantasztik@gmail.com>
2024-05-01 18:38:17 +0200

    make an ingredientAdds function

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     
 
-    async function getDatas(){
-        await fetch('./ingredient.json')
+    function getIngredientData(){
+        fetch('./ingredient.json')
         .then((response) => response.json())
         .then((data) => createButton(data));
     }
     
+    getIngredientData();
 
-    function createButton(data){
+    function createButton(ingredient_data){
 
         let buttons = "";
-        for (let index = 0; index < data.length; index++) {
-            const element = data[index];
+        for (let index = 0; index < ingredient_data.length; index++) {
+            const element = ingredient_data[index];
 
             let button = 
             `
-            <div class="ingredient-button">
-                <img src= ${"./image/ingredients/"+element.imgName+""} alt="">
-                <p>${element.name}</p>
-            </div>
+            <button onclick="ingredientAdds('${element.name}','${element.imgName}')" class="ingredient-button">
+                <img style="width: 40px" src= ${"./image/ingredients/"+element.imgName+""} alt="">
+                <p >${element.name}</p>
+            </button>
             `;
             
             buttons += button;
@@ -30,9 +31,57 @@ document.addEventListener("DOMContentLoaded", ()=>{
         
 
     }
-   
-    getDatas();
+    function getFoodData(){
+        fetch('./food.json')
+        .then((response) => response.json())
+        .then((data) => addToArrayIngredient(data))
+    }
+    
+    getFoodData();
+
+
+    const ingredientNeededArray = [];
+    const foodImgArray = [];
+    
+    function addToArrayIngredient(food_data){
+        for (let index = 0; index < food_data.length; index++) {
+            const element = food_data[index];
+            ingredientNeededArray.push(element.ingredientNeeded);
+            foodImgArray.push(element.imgName);
+            
+        }
+    }
+
+    let index= 1
+    let ingredient_name_array = [];
+    function ingredientAdds(ingredient_name, imagePath){
+       
+       
+        if(ingredient_name_array.length <= 4 ){
+            ingredient_name_array.push(ingredient_name);
+            document.getElementById(index+"").innerHTML  += 
+            `
+            <img style="width: 100px; height: 100px"  src= ${"./image/ingredients/"+imagePath+""} alt="">
+            `
+            index++;
+        }else{
+            
+            alert("Nem lehet négynél több hozzávalót használni!")
+
+        
+           
+        }
+
+        
+        
+        
+
+    }
 
     
+  
+   
 
+    
+    window.ingredientAdds = ingredientAdds;
 })
