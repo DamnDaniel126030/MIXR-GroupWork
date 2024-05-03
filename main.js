4fd7950bef2af16a9cf36fa361905bb7c19c650a
Petrik Diak <diak@petrik.hu>
2024-05-03 09:52:45 +0200

    debug

--- main.js
+++ main.js
@@ -11,13 +11,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
         let buttons = "";
         for (let index = 0; index < ingredient_data.length; index++) {
             const element = ingredient_data[index];
-
             let button = 
             `
-            <button onclick="ingredientAdds('${element.name}','${element.imgName}')" class="ingredient-button">
-                <img style="width: 40px" src= ${"./image/ingredients/"+element.imgName+""} alt="">
-                <p >${element.name}</p>
-            </button>
+                <button onclick="ingredientAdds('${element.name}','${element.imgName}')" class="ingredient-button">
+                    <img style="width: 40px" src= ${"./image/ingredients/"+element.imgName+""} alt="">
+                    <p >${element.name}</p>
+                </button>
             `;
             
             buttons += button;
@@ -51,12 +50,11 @@ document.addEventListener("DOMContentLoaded", ()=>{
             ingredient_name_array.push(ingredient_name);    
             document.getElementById(index+"").innerHTML  += 
             `
-            <img style="width: 100px; height: 100px"  src= ${"./image/ingredients/"+imagePath+""} alt="">
+                <img style="width: 100px; height: 100px"  src= ${"./image/ingredients/"+imagePath+""} alt="">
             `
             index++;
         }
         else{
-            
             alert("Nem lehet négynél több hozzávalót használni!")
         }
     };
@@ -75,13 +73,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
             if(difference.length == 0){
                 document.getElementById("craftedFood").innerHTML = 
                 `
-                <img style="width: 100px; height: 100px"  src= ${"./image/foods/"+element.imgName+""} alt="">
-                <p>${element.name}</p>
+                    <img style="width: 100px; height: 100px"  src= ${"./image/foods/"+element.imgName+""} alt="">
+                    <p>${element.name}</p>
                 `
                 document.getElementById("previouslyFood").innerHTML += 
                 `
-                <img style="width: 100px; height: 100px"  src= ${"./image/foods/"+element.imgName+""} alt="">
-                <p>${element.name}</p>
+                    <img style="width: 100px; height: 100px"  src= ${"./image/foods/"+element.imgName+""} alt="">
+                    <p>${element.name}</p>
                 `
 
                 document.getElementById("1").innerHTML  = "";
@@ -90,7 +88,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 document.getElementById("4").innerHTML  = "";
                     
                 boolean = true;
-            }
+            };
         });
         if(!boolean){
             alert("nincs ilyen opció");
