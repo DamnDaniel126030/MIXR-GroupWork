2530dd0af349665d89bb8ec8f9ff4cbd5c75db26
Petrik Diak <diak@petrik.hu>
2024-05-03 09:08:58 +0200

    added ids to buttons

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    
-
     function getIngredientData(){
         fetch('./ingredient.json')
         .then((response) => response.json())
         .then((data) => createButton(data));
-    }
+    };
     
     getIngredientData();
 
     function createButton(ingredient_data){
-
         let buttons = "";
         for (let index = 0; index < ingredient_data.length; index++) {
             const element = ingredient_data[index];
 
             let button = 
             `
-            <button onclick="ingredientAdds('${element.name}','${element.imgName}')" class="ingredient-button">
+            <button id="${element.id}" onclick="ingredientAdds('${element.name}','${element.imgName}')" class="ingredient-button">
                 <img style="width: 40px" src= ${"./image/ingredients/"+element.imgName+""} alt="">
                 <p >${element.name}</p>
             </button>
@@ -26,37 +23,29 @@ document.addEventListener("DOMContentLoaded", ()=>{
             buttons += button;
         }
         document.getElementById("ingredient").innerHTML = buttons;
+    };
 
-        
-        
-
-    }
     function getFoodData(){
         fetch('./food.json')
         .then((response) => response.json())
         .then((data) => addToArrayIngredient(data))
-    }
+    };
     
     getFoodData();
 
-    
     const ingredientNeededArray = [];
-    
-    
+        
     function addToArrayIngredient(food_data){
         for (let index = 0; index < food_data.length; index++) {
             const element = food_data[index];
             ingredientNeededArray.push(element);
-           
-            
         }
-    }
+    };
     
     let index= 1
     let ingredient_name_array = [];
+
     function ingredientAdds(ingredient_name, imagePath){
-       
-       
         if(ingredient_name_array.length <= 4 ){
             ingredient_name_array.push(ingredient_name);    
             document.getElementById(index+"").innerHTML  += 
@@ -64,15 +53,12 @@ document.addEventListener("DOMContentLoaded", ()=>{
             <img style="width: 100px; height: 100px"  src= ${"./image/ingredients/"+imagePath+""} alt="">
             `
             index++;
-        }else{
             
-            alert("Nem lehet négynél több hozzávalót használni!")
-
-
         }
-        
-
-    }
+        else{
+            alert("Nem lehet négynél több hozzávalót használni!")
+        }
+    };
 
     function mixrButton(){
         let boolean = false;
@@ -81,35 +67,32 @@ document.addEventListener("DOMContentLoaded", ()=>{
             if(element["ingredientNeeded"].length <= ingredient_name_array.length){
                 difference =  ingredient_name_array.filter((ingredientNeeded) => !element["ingredientNeeded"].includes(ingredientNeeded));
                
-            }if(element["ingredientNeeded"].length >= ingredient_name_array.length){
-                
+            }
+            if(element["ingredientNeeded"].length >= ingredient_name_array.length){    
                 difference =  element["ingredientNeeded"].filter((ingredientNeeded) => !ingredient_name_array.includes(ingredientNeeded));
             }
-                    
-                    if(difference.length == 0){
-                
-                        document.getElementById("craftedFood").innerHTML = 
-                        `
-                        <img style="width: 100px; height: 100px"  src= ${"./image/foods/"+element.imgName+""} alt="">
-                        <p>${element.name}</p>
-                        `
-                        document.getElementById("previouslyFood").innerHTML += 
-                        `
-                        <img style="width: 100px; height: 100px"  src= ${"./image/foods/"+element.imgName+""} alt="">
-                        <p>${element.name}</p>
-                        `
+            if(difference.length == 0){
         
-                        
-                        document.getElementById("1").innerHTML  = "";
-                        document.getElementById("2").innerHTML  = "";
-                        document.getElementById("3").innerHTML  = "";
-                        document.getElementById("4").innerHTML  = "";
-                    
-                        
-                        boolean = true;
-                }
+                document.getElementById("craftedFood").innerHTML = 
+                `
+                <img style="width: 100px; height: 100px"  src= ${"./image/foods/"+element.imgName+""} alt="">
+                <p>${element.name}</p>
+                `
+                document.getElementById("previouslyFood").innerHTML += 
+                `
+                <img style="width: 100px; height: 100px"  src= ${"./image/foods/"+element.imgName+""} alt="">
+                <p>${element.name}</p>
+                `
+
+                
+                document.getElementById("1").innerHTML  = "";
+                document.getElementById("2").innerHTML  = "";
+                document.getElementById("3").innerHTML  = "";
+                document.getElementById("4").innerHTML  = "";
             
-            
+                
+                boolean = true;
+            };
         });
         if(!boolean){
             alert("nincs ilyen opció");
@@ -122,15 +105,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }else{
             index= 1
             ingredient_name_array = [];
-        }
-           
+        }        
+    };
 
-        
-    }
 
-    
-  
-   
 
     window.mixrButton = mixrButton;
     window.ingredientAdds = ingredientAdds;
